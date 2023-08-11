4727efc1f7030badcacb7a74403239cfbd17f57a
Akshat Rai Laddha <laddhaakshatrai@gmail.com>
2023-08-11 05:49:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,73 +1,71 @@
-// create web server with express
-const express = require('express');
-const app = express();
+// Create web server 
+var express = require("express");
+var router = express.Router();
+var Comment = require("../models/comment");
+var Campground = require("../models/campground");
+var middleware = require("../middleware");
 
-// create application/x-www-form-urlencoded parser
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// =================
+// COMMENTS ROUTES
+// =================
 
-// set up handlebars view engine
-const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
-// set up MongoDB
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
-let dbo;
-
-// connect to MongoDB
-MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
-    if (err) throw err;
-    dbo = db.db("comments");
-    console.log("Connected to MongoDB");
-});
-
-// set up static files
-app.use(express.static(__dirname + '/public'));
-
-// set up port
-app.set('port', process.env.PORT || 3000);
-
-// set up route
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
-// set up post request
-app.post('/post', urlencodedParser, function (req, res) {
-    // get the name and comment from the form
-    let name = req.body.name;
-    let comment = req.body.comment;
-    // create a new document
-    let myobj = { name: name, comment: comment };
-    // insert the document to the collection
-    dbo.collection("comments").insertOne(myobj, function (err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-    });
-    // redirect to the home page
-    res.redirect('/');
-});
-
-// set up get request
-app.get('/get', function (req, res) {
-    // find all documents in the collection
-    dbo.collection("comments").find({}).toArray(function (err, result) {
-        if (err) throw err;
-        // render the comments page and pass the data
-        res.render('comments', { comments: result });
+// Comments new
+router.get("/campgrounds/:id/comments/new", middleware.isLoggedIn, function (req, res) {
+    // find campground by id
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) console.log(err);
+        else res.render("comments/new", { campground: campground });
     });
 });
 
-// set up 404 page
-app.use(function (req, res) {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
+// Comments create
+router.post("/campgrounds/:id/comments", middleware.isLoggedIn, function (req, res) {
+    // lookup campground using ID
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        }
+        // create new comment
+        else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) console.log(err);
+                else {
+                    // add username and id to comment
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    // save comment
+                    comment.save();
+                    // connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    // redirect to campground show page
+                    req.flash("success", "Successfully added comment.");
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
 });
 
-// set up 500 page
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.type('text/plain');
+// Comments edit
+router.get("/campgrounds/:id/comments/:comment_id/edit", middleware.checkCommentOwnership, function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if (err) console.log(err);
+        else res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+    });
+});
+
+// Comments update
+router.put("/campgrounds/:id/comments/:comment_id", middleware.checkCommentOwnership, function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
+        if (err) console.log(err);
+        else {
+            req.flash("success", "Successfully edited comment.");
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// Comments destroy
+router.delete("/campgrounds/:id/comments/:comment_id", middleware
